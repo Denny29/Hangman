f051b208f672c65fbd2690a50c1a7b892b7741a6
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-06-26 22:09:05 -0400

    The hidden word doesn't appear on the page. At least not when it changes.

--- src/guess.js
+++ src/guess.js
@@ -1,36 +1,35 @@
 import { useState } from "react";
 let guessVar = " ";
-
+let hiddenWord;
 export default function Guess(props) {
   const [pastGuess, setPastGuess] = useState([]);
+  hiddenWord = props.word;
+  const [hidden, setHidden] = useState(hiddenWord);
 
   function guessFunc() {
     guessVar = document.getElementById("guess").value;
+    guessVar = guessVar.toUpperCase();
     setPastGuess(pastGuess + " " + guessVar);
     checkGuess();
   }
-  function checkGuess(par) {
+
+  function checkGuess() {
     //checks if the guessed letter is equal to any letter in secret word
     for (let i2 = 0; i2 < props.secret.length; i2++) {
       if (props.secret[i2] === guessVar) {
-        console.log(true + " secretLetter: " + props.secret[i2]);
+        hiddenWord[i2] = guessVar;
+        setHidden(hiddenWord);
+        console.log(true + hidden);
+        // console.log(true + " secretLetter: " + props.secret[i2]);
       } else {
         console.log(false + " secretLetter: " + props.secret[i2]);
       }
     }
   }
 
-  let wordArray = props.secret.split("");
-  let hiddenWord = "";
-
-  for (let i = 1; i <= wordArray.length; i++) {
-    //Loop makes a word with underscores EX: Cat = _ _ _
-    hiddenWord += " _";
-  }
-
   return (
     <div>
-      {hiddenWord}{" "}
+      <p> {hidden} </p>
       <div>
         <input id="guess" placeholder="Guess a letter" />
         <button onClick={guessFunc}>Click Me</button>
--- src/hiddenWord.js
+++ src/hiddenWord.js
@@ -2,18 +2,24 @@ import { useState } from "react";
 import Guess from "./guess.js";
 import Man from "./man.js";
 let wordVar = " ";
-
+let hiddenWord = [];
 export default function Word(props) {
   const [secretWord, setSecretWord] = useState(" ");
 
   function getInputValue() {
     wordVar = document.getElementById("hiddenWord").value;
-    setSecretWord(wordVar);
+    setSecretWord(wordVar.toUpperCase());
+    makeHidden();
   }
-
+  function makeHidden() {
+    for (let i = 0; i < wordVar.length; i++) {
+      //Loop makes a word with underscores EX: Cat = _ _ _
+      hiddenWord.push("_ ");
+    }
+  }
   if (secretWord !== " ") {
     console.log(secretWord);
-    return <Guess secret={secretWord} />;
+    return <Guess secret={secretWord} word={hiddenWord} />;
   } else {
     return (
       <div>
